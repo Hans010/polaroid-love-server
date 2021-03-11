1f5ce78fb63785619179daa42ec2e727b6b33be4
hans010 <ricardohans@gmail.com>
2021-03-11 22:24:00 +0000

    new repository old project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Polaroid Love (Server)
+
+_This project is part of the **"You have to start somewhere"** React Project Series_
+
+A simple project with "polaroids" created by users.
+New polaroids can be created with a form.
+User can like, edit and delete polaroids.
+
+App was created with MERN stack and is separated between client and server.
+Server is connected to a Mongo cluster.
+In order to run this app, you will need to provide a connection to a Mongo db cluster.\
+Create an `.env` file and add the connection url.\
+(Check `.envexample` for reference)
+
+### Currently in development
+- Authentication
+- User profile
+
+## Installation
+
+######Install dependencies
+```sh
+npm install
+```
+
+######Start Client
+```sh
+npm start
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+// all requests with the prefix 'posts' will be handled by postRoutes
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello from mern1!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(err => console.log(err));
+
+mongoose.set('useFindAndModify', false);
+
