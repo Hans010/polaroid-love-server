8545991986f1ddb5e102df136488fc4655752ea7
hans010 <ricardohans@gmail.com>
2021-03-17 13:22:10 +0000

    Setting app for deployment

--- index.js
+++ index.js
@@ -20,18 +20,14 @@ app.use('/user', userRoutes);
 const hi = process.env.CONNECTION_URL || 'no url here man';
 
 app.get('/', (req, res) => {
-    res.send(<div>
-            <p>'Hello from Polaroid Server 1'</p>
-        {hi}
-        </div>
-    );
+    res.send(hi)
 });
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-//     .catch(err => console.log(err));
-//
-// mongoose.set('useFindAndModify', false);
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch(err => console.log(err));
+
+mongoose.set('useFindAndModify', false);
 
